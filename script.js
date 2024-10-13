c35d073f85d1a8ccf5bdc7b513c2c25dc89e9298
Mckarov26 <[166902834+mckarov26@users.noreply.github.com]>
2024-10-13 14:59:43 +1300

    Created getHumanChoice and playRound functions
    - Declare the players score variables in Global Scope
    - In getHumanChoice function takes the user choice and returns it.
    - In playRound function takes  the human and computer player choices as arguments,
      plays a single round, increments the round winner's score and logs a winner announcement.

--- script.js
+++ script.js
@@ -9,14 +9,61 @@ const getComputerChoice = () => {
             return "rock";
             break;
         case 2: 
-            return ("paper");
+            return "paper";
             break;
         case 3:
-            return ("scissors");
+            return "scissors";
             break;  
         default:
             
     }
 }
 
+const getHumanChoice = () => {
+    let yourChoice = prompt("Please Choose rock, paper or scissors");
+    let humanChoice = yourChoice.toLowerCase();
+
+    switch(humanChoice) {
+        case "rock":
+            return "rock";
+            break;
+        case "paper":
+            return "paper";
+            break;
+        case "scissors":
+            return "scissors";
+            break;
+        default:
+            return "Invalid! Please Choose rock, paper or scissors";
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+const playRound = (humanChoice, computerChoice) => {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        console.log(`It's a tie! You both chose ${humanChoice}.`);
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        console.log(`You win!, ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+        
+    } else if (
+        (computerChoice === "rock" && humanChoice === "scissors") ||
+        (computerChoice === "paper" && humanChoice === "rock") ||
+        (computerChoice === "scissors" && humanChoice === "paper")
+    ) {
+        console.log(`Computer win! ${computerChoice} beats ${humanChoice}`);
+        computerChoice++;
+    } else {
+        console.log("Invalid input!");
+    }
+}
 
