1e3dffbb355916164aac4bf5441ccc3362fdd5d7
Mckarov26 <[166902834+mckarov26@users.noreply.github.com]>
2024-10-13 14:12:20 +1300

    I created a getComputerChoice function
    This will randomly return one of the following string values: “rock”, “paper” or “scissors”.

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //This is the Javascript File!
 //Code below! 
+
+const getComputerChoice = () => {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+
+    switch(computerChoice) {
+        case 1: 
+            return "rock";
+            break;
+        case 2: 
+            return ("paper");
+            break;
+        case 3:
+            return ("scissors");
+            break;  
+        default:
+            
+    }
+}
+
+
