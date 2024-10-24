c6724fd550181d3253374450ab751296c0352fd8
Mckarov26 <[166902834+mckarov26@users.noreply.github.com]>
2024-10-24 23:48:27 +1300

    modified index.html, script.js and added style.css

--- script.js
+++ script.js
@@ -26,57 +26,99 @@ const getComputerChoice = () => {
 
 let humanScore = 0;
 let computerScore = 0;
+let roundNum = 1;
+let maxRoundNum = 5;
 
 
 const playRound = (humanChoice, computerChoice) => {
     
 
-    const resultDiv = document.getElementById("results");
+    const playGame = document.getElementById("playRound");
+    const info = document.getElementById("info");
    
   
    
 
    
 
-    if (humanChoice === computerChoice) {
+    if (humanChoice === computerChoice && roundNum <= maxRoundNum) {
+        
+        
         humanScore++;
         computerScore++;
-       resultDiv.textContent = `It's a tie! You both chose ${humanChoice}. Your score is ${humanScore} and Computer score is ${computerScore}`;
+        playGame.textContent = `Round ${roundNum}`;
+        info.textContent = `You both chose ${humanChoice}. Your score is ${humanScore} and Computer score is ${computerScore}`;
         
     } else if (
-        (humanChoice === "rock" && computerChoice === "scissors") ||
-        (humanChoice === "paper" && computerChoice === "rock") ||
-        (humanChoice === "scissors" && computerChoice === "paper")
+        (humanChoice === "rock" && computerChoice === "scissors" && roundNum <= maxRoundNum) ||
+        (humanChoice === "paper" && computerChoice === "rock" && roundNum <= maxRoundNum) ||
+        (humanChoice === "scissors" && computerChoice === "paper" && roundNum <= maxRoundNum)
     ) {
+        
         humanScore++;
-        resultDiv.textContent = `You win!, ${humanChoice} beats ${computerChoice}. Your score is ${humanScore} and Computer score is ${computerScore}`;
+        playGame.textContent = `Round ${roundNum}`;
+        info.textContent = `${humanChoice} beats ${computerChoice}. Your score is ${humanScore} and Computer score is ${computerScore}`;
         
         
     } else if (
-        (computerChoice === "rock" && humanChoice === "scissors") ||
-        (computerChoice === "paper" && humanChoice === "rock") ||
-        (computerChoice === "scissors" && humanChoice === "paper")
+        (computerChoice === "rock" && humanChoice === "scissors" && roundNum <= maxRoundNum) ||
+        (computerChoice === "paper" && humanChoice === "rock" && roundNum <= maxRoundNum) ||
+        (computerChoice === "scissors" && humanChoice === "paper" && roundNum <= maxRoundNum)
     ) {
+        
         computerScore++;
-        resultDiv.textContent = `Computer win! ${computerChoice} beats ${humanChoice}. Your score is ${humanScore} and Computer score is ${computerScore}`;
+        playGame.textContent = `Round ${roundNum}`;
+        info.textContent = `${computerChoice} beats ${humanChoice}. Your score is ${humanScore} and Computer score is ${computerScore}`;
 
         
     } else {
-        console.log("Invalid! Please Choose rock, paper or scissors");
+        info.textContent = `5 Rounds is Over, Do you wanna Play Again? `
     }
 
-
     
-
- 
+    roundNum++;
     
 }
+   
+    
+const checkWinner = () => {
+
+    const whoWins = document.getElementById("checkWinner");
+
+    if (roundNum > maxRoundNum) {
+
+        if (humanScore > computerScore) {
+            whoWins.textContent = "You win in this game. Good Job!";
+        } else if (humanScore < computerScore) {
+            whoWins.textContent = "You lose in this game. Try again next time!";
+        } else if (humanScore === computerScore) {
+            whoWins.textContent = "Tie! Human and Computer Wins!";
+        }
+
+    }
+    
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
 document.getElementById("rock").addEventListener("click", () => {
     const humanChoice = "rock";
     const computerChoice = getComputerChoice();
     playRound(humanChoice, computerChoice);
+    checkWinner();
 });
 
 
@@ -84,12 +126,14 @@ document.getElementById("paper").addEventListener("click", () => {
     const humanChoice = "paper";
     const computerChoice = getComputerChoice();
     playRound(humanChoice, computerChoice);
+    checkWinner();
 } )
 
 document.getElementById("scissors").addEventListener("click", () => {
     const humanChoice = "scissors";
     const computerChoice = getComputerChoice();
     playRound(humanChoice, computerChoice);
+    checkWinner();
 })
 
 
